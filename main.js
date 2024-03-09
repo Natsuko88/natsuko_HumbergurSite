e2045493fb9b30c738b083e9d21edbce20d2c9da
AyumiYamamura-cloud <138938774+AyumiYamamura-cloud@users.noreply.github.com>
2024-03-09 16:01:01 +0900

    ファイル更新0309

--- main.js
+++ main.js
@@ -16,17 +16,29 @@ mBtn.addEventListener('click',()=> {
         lSide.classList.toggle('is-active');
         mBtnSide.classList.toggle('is-active');
         bLayer.classList.toggle('is-active');
-       // wrapperMain.classList.toggle('is-active');
-       // body.classList.toggle('is-active');
+        //wrapperMain.classList.toggle('is-active');
+        body.classList.toggle('is-active');
     });
 
 //リサイズ
-    window.addEventListener('resize', function(){
-        mBtn.classList.remove('is-active');
-        lSide.classList.remove('is-active');
-        mBtnSide.classList.remove('is-active');
-        bLayer.classList.remove('is-active');
-       // wrapperMain.classList.remove('is-active');
-        //body.classList.remove('is-active');
-    });
+    document.addEventListener('DOMContentLoaded', function(){
+        //条件分岐の判断に利用するメディアクエリを指定
+        const mq=window.matchMedia("(min-width:1025px)");
+        //メディアクエリをチェックする関数を定義して実行
+        function mqCheck(mq){
+            //if PCサイズ（１０２５px以上の場合）
+            if(mq.matches){
+                mBtn.classList.remove('is-active');
+                lSide.classList.remove('is-active');
+                mBtnSide.classList.remove('is-active');
+                bLayer.classList.remove('is-active');
+                body.classList.remove('is-active');
+            }else{
+                //else tabサイズ以下（１０２４px以下の場合）
+            }  
+        }
+        mqCheck(mq);
+        // MediaQueryListが持つ情報が変更された場合に、定義した関数を実行
+        mq.addEventListener('change', mqCheck, false);
+}, false);
   
